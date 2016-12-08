f0c67540339a9cff90ca14ff5425ed327cbf8822
Sam Olds <samolds@gmail.com>
2016-12-07 21:08:39 -0700

    added logic to swap the top 20% hottest keys from existing servers to cold server

--- src/experiments/4hotkey/client.go
+++ src/experiments/4hotkey/client.go
@@ -123,18 +123,97 @@ func main() {
 // figure out which keys have been remapped to the new server. and once we've
 // figured out the hottest ~20 keys or something that have been remapped, we
 // just set those on the new server
-func catchUpColdServer(old_mc *memcached.Client, new_mc *memcached.Client,
+func catchUpColdServer(old_mc *memcache.Client, new_mc *memcache.Client,
 	key_distribution map[string]int) error {
-	// we know the cold server is going to be the 4th on in the new server list
 
-	// TODO(sam): wip
-	hot_key_servers, err := common.GetHotKeysPerServer(mc, key_distribution)
+	// get all of the server objects from the memcache client
+	servers, err := new_mc.Servers.Servers()
 	if err != nil {
 		return err
 	}
 
-	for _, hot_key_server := range hot_key_servers {
-		hot_key_server.KeyDistribution
+	// we know the cold server is going to be the 4th on in the new server list
+	new_server_addr := servers[3].String()
+
+	// keys that are now on the new cold server
+	// using map[string]struct{} because it's easier to check if key is in a map
+	// rather than a slice, and the struct{} provides no additional memory
+	// requirements
+	remapped_keys := make(map[string]struct{})
+
+	// find all of the keys that have been remapped to the new server
+	for key, _ := range key_distribution {
+		server, err := new_mc.Servers.PickServer(key)
+		if err != nil {
+			return err
+		}
+
+		if server.String() == new_server_addr {
+			var empty struct{}
+			remapped_keys[key] = empty
+		}
+	}
+
+	// get the hottest keys on each of the original server
+	hot_key_servers, err := common.GetHotKeysPerServer(old_mc,
+		key_distribution)
+	if err != nil {
+		return err
+	}
+
+	// read in a file that contains the value that will be set for every memcache
+	// key/value pair
+	memcache_value, err := ioutil.ReadFile(common.MEMCACHE_VALUE_FILENAME)
+	if err != nil {
+		return err
+	}
+
+	// function to move a key from one server to another if it exists and return
+	// "how hot" that key was relative to the other keys it knows about
+	remap_key_from_old_to_new := func(server_index int, index int) (float64, bool) {
+		if len(hot_key_servers[server_index].KeyDistribution) < index {
+			return 0.0, true
+		}
+
+		key_dist := hot_key_servers[server_index].KeyDistribution[index]
+		_, key_needs_remapping := remapped_keys[key_dist.Key]
+		if key_needs_remapping {
+			// TODO(sam): add sleep or something to indicate that server needs to go
+			// get the value from the old server
+			new_mc.Set(&memcache.Item{Key: key_dist.Key, Value: memcache_value})
+			percentage := float64(key_dist.Value) / float64(
+				hot_key_servers[server_index].TotalKeyHitCount)
+			return percentage, false
+		}
+		return 0.0, false
+	}
+
+	total_key_hit_percentage := 0.0
+	index := 0
+
+	// iterate through all of the keys on each existing server from hottest to
+	// coldest until the new server has 20% of the hottest keys from the old
+	// servers
+	for total_key_hit_percentage < 20.0 {
+		key_hit_percentage, too_much := remap_key_from_old_to_new(0, index)
+		if too_much {
+			break
+		}
+		total_key_hit_percentage += key_hit_percentage
+
+		key_hit_percentage, too_much = remap_key_from_old_to_new(1, index)
+		if too_much {
+			break
+		}
+		total_key_hit_percentage += key_hit_percentage
+
+		key_hit_percentage, too_much = remap_key_from_old_to_new(2, index)
+		if too_much {
+			break
+		}
+		total_key_hit_percentage += key_hit_percentage
+
+		index++
 	}
 
 	return nil
